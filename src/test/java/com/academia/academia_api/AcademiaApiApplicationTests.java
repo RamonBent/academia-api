7c00cf75f5bb51a6264193db4d0178b46fe17c85
Ramon Bento <104931916+RamonBent@users.noreply.github.com>
2025-04-08 16:16:32 -0300

    Add files via upload
    Iniciando projeto

---
+++ src/test/java/com/academia/academia_api/AcademiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.academia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
