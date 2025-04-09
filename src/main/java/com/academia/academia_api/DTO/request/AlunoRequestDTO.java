44bb481bb8bd638338d854b504533c74cbc8fd74
ramon <ramon.bento.oliver@gmail.com>
2025-04-09 18:42:41 -0300

    Criando DTO e Mappers

--- src/main/java/com/academia/academia_api/DTO/request/AlunoRequestDTO.java
+++ src/main/java/com/academia/academia_api/DTO/request/AlunoRequestDTO.java
@@ -1,5 +1,19 @@
 package com.academia.academia_api.DTO.request;
 
+import lombok.Data;
 
+import java.time.LocalDate;
+
+@Data
 public class AlunoRequestDTO {
+        private Long id;
+        private String nome;
+        private LocalDate dataNascimento;
+        private String telefone;
+        private String email;
+        private String endereco;
+        private String plano;
+        private Long instrutorId;
 }
+
+
